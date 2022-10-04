fb3c0c00749d836a01821560dcf6ffd30151d805
potnuruavinash111@gmail.com <potnuruavinash111@gmail.com>
2022-10-04 17:28:26 +0530

    react_music_player

---
+++ src/Player/audios.js
@@ -0,0 +1,52 @@
+exports.songsdata = [
+
+    {
+        "title":"Justin Bieber - Love Yourself",
+        "url":"https://www.mboxdrive.com/KTZ.mp3"
+
+    },
+
+    {
+        "title": "Drake - Forever",
+        "url": "https://beardbarnmusicbucket.s3.amazonaws.com/The+Wild+Horse"
+    },
+    {
+        "title": "Linking Park - In the end",
+        "url": "https://beardbarnmusicbucket.s3.amazonaws.com/The+Wild+Horse"
+    },
+    {
+        "title": "Travis Scott - Stop trina be God",
+        "url": "https://beardbarnmusicbucket.s3.amazonaws.com/The+Wild+Horse"
+    },
+    {
+        "title":"Vikram",
+        "url":"https://www.mboxdrive.com/vikram.mp3"
+
+    },
+    {
+        "title":"Sy Mehdi - Finito",
+        "url":"https://www.mboxdrive.com/MOLLBEN64.mp3"
+
+    },
+    {
+        "title":"Красное дерево Кишки",
+        "url":"https://www.mboxdrive.com/musora.mp3"
+
+    },
+    {
+        "title":"MONO, Onionn Waiting For You",
+        "url":"https://www.mboxdrive.com/son4.mp3"
+
+    },
+    {
+        "title":"Doin Time",
+        "url":"https://www.mboxdrive.com/sadboy1.mp3"
+
+    },
+    {
+        "title":"She Drives Me Crazy",
+        "url":"https://www.mboxdrive.com/Fine%20Young%20Cannibals%20-%20She%20Drives%20Me%20Crazy%20(1989)%20(URBANOMP3.NET).mp3"
+
+    }
+    
+]
